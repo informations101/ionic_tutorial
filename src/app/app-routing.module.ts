dcf06610dead22f63309d590d3fe8e76e8dbf3c2
Sophea PHOS <emailsophea@gmail.com>
2019-09-09 15:02:20 +0700

    login01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,40 @@
+
+// { path: '', redirectTo: 'home', pathMatch: 'full' },
+// { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+// { path: 'crud-storage', loadChildren: './pages/crud-storage/crud-storage.module#CrudStoragePageModule' },
+
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
- 
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'crud-storage', loadChildren: './pages/crud-storage/crud-storage.module#CrudStoragePageModule' },
+  // { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  {
+    path: 'admin-dashboard',
+    loadChildren: './pages/admin-dashboard/admin-dashboard.module#AdminDashboardPageModule',
+    canActivate: [AuthGuard],
+    data: {
+      role: 'ADMIN'
+    }
+  },
+  {
+    path: 'user-dashboard',
+    loadChildren: './pages/user-dashboard/user-dashboard.module#UserDashboardPageModule',
+    canActivate: [AuthGuard],
+    data: {
+      role: 'USER'
+    }
+  }
 ];
- 
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
